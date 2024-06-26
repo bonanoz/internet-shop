66ac06738475bad0f7a98aef98ea64647e9fc34c
bonanoz <makar2109qwerty@mail.ru>
2024-06-26 19:42:35 +0400

    еее

--- internetshop/shop/admin.py
+++ internetshop/shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Product
+class ProductAdmin(admin.ModelAdmin):
+    pass
+# Register your models here
+admin.site.register(Product, ProductAdmin)
