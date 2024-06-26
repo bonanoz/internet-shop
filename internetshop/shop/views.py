04e3b78c01713109d66ac40c285e53c265483fa7
bonanoz <makar2109qwerty@mail.ru>
2024-06-26 20:20:31 +0400

    еее

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Product
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    return render(request, "index.html", {
+        'products': products
+    })
